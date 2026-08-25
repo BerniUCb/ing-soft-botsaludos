19cc6d1d3aa87edc3772b3761a1eb24a1fdebc20
BerniUCB <bernardo.rios@ucb.edu.bo>
2026-08-25 00:20:52 -0400

    feat: saludo en ingles

--- src/saludador.js
+++ src/saludador.js
@@ -1,35 +1,51 @@
-function saludar(nombre, hora, genero, edad,idioma) {
+function saludar(nombre, hora, genero, edad, idioma) {
   let saludo;
   let bienvenida;
   let tratamiento = "";
 
-if (idioma === "es") {
-  if (hora >= 6 && hora < 12) {
-    saludo = "Buenos días";
-  } else if (hora >= 12 && hora < 19) {
-    saludo = "Buenas tardes";
-  } else {
-    saludo = "Buenas noches";
-  }
-
-
-  if (genero === "M") {
-    bienvenida = "bienvenido";
-  } else {
-    bienvenida = "bienvenida";
-  }
-
-
-  if (edad > 30) {
-    if (genero === "M") {
-      tratamiento = "Sr. ";
+  if (idioma === "es") {
+    if (hora >= 6 && hora < 12) {
+      saludo = "Buenos días";
+    } else if (hora >= 12 && hora < 19) {
+      saludo = "Buenas tardes";
     } else {
-      tratamiento = "Sra. ";
+      saludo = "Buenas noches";
+    }
+
+    if (genero === "M") {
+      bienvenida = "bienvenido";
+    } else {
+      bienvenida = "bienvenida";
+    }
+
+    if (edad > 30) {
+      if (genero === "M") {
+        tratamiento = "Sr. ";
+      } else {
+        tratamiento = "Sra. ";
+      }
+    }
+  } else if (idioma === "en") {
+    if (hora >= 6 && hora < 12) {
+      saludo = "Good morning";
+    } else if (hora >= 12 && hora < 19) {
+      saludo = "Good afternoon";
+    } else {
+      saludo = "Good evening";
+    }
+
+    bienvenida = "welcome";
+
+    if (edad > 30) {
+      if (genero === "M") {
+        tratamiento = "Mr. ";
+      } else {
+        tratamiento = "Mrs. ";
+      }
     }
   }
-}
 
   return saludo + " " + tratamiento + nombre + ", " + bienvenida;
 }
 
-export default saludar;     
+export default saludar;
--- src/saludador.test.js
+++ src/saludador.test.js
@@ -1,15 +1,23 @@
 import saludar from "./saludador.js";
 
 describe("Saludador", () => {
-  it("deberia saludar en español sin tratamiento a un menor de 30", () => {
-    expect(saludar("Berni", 9, "M", 20, "es")).toEqual("Buenos días Berni, bienvenido");
-  });
-
   it("deberia saludar en español con Sr a un hombre mayor de 30", () => {
     expect(saludar("Juan", 9, "M", 45, "es")).toEqual("Buenos días Sr. Juan, bienvenido");
   });
 
   it("deberia saludar en español con Sra a una mujer mayor de 30", () => {
     expect(saludar("Ana", 15, "F", 50, "es")).toEqual("Buenas tardes Sra. Ana, bienvenida");
+  });
+
+  it("deberia saludar en ingles a un menor de 30", () => {
+    expect(saludar("Berni", 9, "M", 20, "en")).toEqual("Good morning Berni, welcome");
+  });
+
+  it("deberia saludar en ingles con Mr a un hombre mayor de 30", () => {
+    expect(saludar("John", 15, "M", 45, "en")).toEqual("Good afternoon Mr. John, welcome");
+  });
+
+  it("deberia saludar en ingles con Mrs a una mujer mayor de 30", () => {
+    expect(saludar("Mary", 22, "F", 60, "en")).toEqual("Good evening Mrs. Mary, welcome");
   });
 });
