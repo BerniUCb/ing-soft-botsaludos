b5fff2cf2e4e2c1061137a661c2e2b7057045d7b
BerniUCB <bernardo.rios@ucb.edu.bo>
2026-08-24 23:48:48 -0400

    feat: saludar por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -2,7 +2,8 @@ import saludar from "./saludador";
 
 const saludarButton = document.querySelector("#saludar-button");
 const div = document.querySelector("#resultado-div");
+const nombre = document.querySelector("#nombre");
 
 saludarButton.addEventListener("click", () => {
-  div.innerHTML = "<p>" + saludar() + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre.value) + "</p>";
 });
--- src/saludador.test.js
+++ src/saludador.test.js
@@ -1,7 +1,7 @@
 import saludar from "./saludador.js";
 
 describe("Saludador", () => {
-  it("deberia saludar con hola", () => {
-    expect(saludar()).toEqual("Hola");
+  it("deberia saludar con hola y el nombre", () => {
+    expect(saludar("Berni")).toEqual("Hola Berni");
   });
 });
